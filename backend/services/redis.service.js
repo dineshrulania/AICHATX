0cd14966df8967ab8be5252b4263e4bfb0a157d2
Dinesh Rulania <dineshrulania@users.noreply.github.com>
2026-06-12 23:57:26 +0530

    Support redis connection string URL in REDIS_HOST

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -1,15 +1,18 @@
 import Redis from 'ioredis';
 
+const redisHost = process.env.REDIS_HOST || '';
+const isUri = redisHost.startsWith('redis://') || redisHost.startsWith('rediss://');
 
-const redisClient = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD
-});
-
+const redisClient = isUri
+    ? new Redis(redisHost)
+    : new Redis({
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWORD
+    });
 
 redisClient.on('connect', () => {
     console.log('Redis connected');
-})
+});
 
 export default redisClient;
